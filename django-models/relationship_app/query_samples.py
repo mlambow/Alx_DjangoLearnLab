2c3aa58cdb7c46e7d33bf2a0e78762a7f257bd8a
Wandile Mlambo <wandile.mlambo@yahoo.com>
2024-08-17 10:30:08 +0200

    query sets

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -1,3 +1,10 @@
 from .models import Book, Librarian, Library
 
-books = Book.objects.get()
+def books_by_author(author_name):
+    author = Book.objects.get(name=author_name)
+    books = 
+
+def books_in_library(library_name):
+     library_books = Library.objects.get(name=library_name) 
+     books = books.all()
+     return books
