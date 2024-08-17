e8b0d560cf853a1c8054aaef982730e784df814d
Wandile Mlambo <wandile.mlambo@yahoo.com>
2024-08-17 11:35:09 +0200

    library

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from .models import Book
+from .models import Book, Library
 
 def list_book(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
+
+def library_details(request):
+    library = Library.objects.all()
+    return render(request, 'relationship_app/library_detail.html', library)
