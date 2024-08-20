da27483766f5b94f2ca51dec2f311643bd5843ac
Wandile Mlambo <wandile.mlambo@yahoo.com>
2024-08-20 14:02:57 +0200

    views

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('books/', list_books, name='list_book'),
-    path('library/<int:pk>/', LibraryDetailView, name='library_detail')
+    path('library/<int:pk>/', LibraryDetailView, name='library_detail'),
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView, LogoutView
+from django.shortcuts import redirect, render
 
 def list_books(request):
     books = Book.objects.all()
@@ -12,3 +16,15 @@ class LibraryDetailView(DetailView):
     library = Library.objects.all()
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+#Registration view
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('list_books')
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
