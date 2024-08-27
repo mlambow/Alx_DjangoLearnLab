2c2930d3fd670080b3fa54a3b3fa50cb301f0a66
Wandile Mlambo <wandile.mlambo@yahoo.com>
2024-08-27 14:51:45 +0200

    api

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -2,9 +2,9 @@ from django.urls import path, include
 from .views import BookList
 from rest_framework.routers import DefaultRouter
 
-router = DefaultRouter()
-router.register(r'book', BookList)
+api = DefaultRouter()
+api.register(r'book', BookList)
 
 urlpatterns = [
-    path('api/', include(router.urls))
+    path('api/', include(api.urls))
 ]
