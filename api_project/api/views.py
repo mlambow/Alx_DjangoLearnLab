f83cc1df01b7a88a32733afa85961fc7b40fd31f
Wandile Mlambo <wandile.mlambo@yahoo.com>
2024-09-01 13:08:00 +0200

    perm

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from rest_framework import generics, viewsets
 from .serializers import BookSerializer
 from .models import Book
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.views import APIView 
 
 class BookList(generics.ListAPIView):
     serializer_class = BookSerializer
