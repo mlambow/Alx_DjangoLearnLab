53a99343bd24705ab810b403590b6afc3bd86709
Wandile Mlambo <wandile.mlambo@yahoo.com>
2024-09-15 23:32:05 +0200

    attrs

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -13,3 +13,10 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['content']
+
+class TagWidget():
+    def __init__(self, attrs=None):
+        default_attrs = {'placeholder': 'Enter tags separated by commas'}
+        if attrs:
+            default_attrs.update(attrs)
+        super().__init__(attrs=default_attrs)
