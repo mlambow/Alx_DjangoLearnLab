a048e2cc4726dc24b433dfec6759309036a46b14
Wandile Mlambo <wandile.mlambo@yahoo.com>
2024-09-15 23:40:42 +0200

    tagwidget

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,7 +2,14 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from taggit.forms import TagWidget as Widget
-from .models import Comment
+from .models import Comment, Post
+
+class TagWidget():
+    def __init__(self, attrs=None):
+        default_attrs = {'placeholder': 'Enter tags separated by commas'}
+        if attrs:
+            default_attrs.update(attrs)
+        super().__init__(attrs=default_attrs)
 
 class SignUpForm(UserCreationForm):
     email = forms.EmailField()
@@ -10,14 +17,13 @@ class SignUpForm(UserCreationForm):
         model = User
         fields = ('username', 'email', 'password1', 'password2')
 
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content', 'tags']
+        widgets = {'tags': TagWidget()}
+
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['content']
-
-class TagWidget(Widget):
-    def __init__(self, attrs=None):
-        default_attrs = {'placeholder': 'Enter tags separated by commas'}
-        if attrs:
-            default_attrs.update(attrs)
-        super().__init__(attrs=default_attrs)
