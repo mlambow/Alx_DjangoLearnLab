00601fa9c2c6b591e64435e1045017391bd373d5
Wandile Mlambo <wandile.mlambo@yahoo.com>
2024-09-16 13:47:58 +0200

    comment

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -7,3 +7,6 @@ class Post(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+class Comment(models.Model):
+    
