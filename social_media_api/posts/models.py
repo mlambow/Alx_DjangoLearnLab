4ae7fac467f0960c490a9bcef7603bff8d9d361f
Wandile Mlambo <wandile.mlambo@yahoo.com>
2024-09-16 19:46:38 +0200

    viewsets, pagination, filters

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -8,3 +8,12 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Comment by {self.author.username} on {self.post.title}'
--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework import viewsets, pagination, filters
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from .models import Comment, Post
+from .serializers import CommentSerializer, PostSerializer
 
-# Create your views here.
+class SetPagination(pagination.PageNumberPagination):
+    page_size = 20
+    page_query_param = 'page_size'
+    max_page_size = 100
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    pagination_class = SetPagination
+    filter_backends = [filters.SearchFilter]
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticated]
+    pagination_class = SetPagination
+    filter_backends = [filters.SearchFilter]
