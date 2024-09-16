a696d0805da9f73c6fd9a271d81071543737a2f7
Wandile Mlambo <wandile.mlambo@yahoo.com>
2024-09-16 12:19:14 +0200

    AbstractUser

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -1,7 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractUser
 
-class User(AbstractBaseUser):
+class User(AbstractUser):
     bio = models.TextField()
     profile_picture = models.ImageField()
     followers = models.ManyToManyField('self', symmetrical=False)
